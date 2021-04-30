706822a80358a3c8732e712693c2ad47ecee053a
CharlesCarley <snailrose@gmail.com>
2021-04-30 04:43:27 -0600

    Added another CMake macro to define extended properties.

--- ExternalTarget.cmake
+++ ExternalTarget.cmake
@@ -32,15 +32,16 @@ endmacro()
 
 
 
-macro(add_external_test NAME DIR )
-    unset(${NAME}Test)
-    unset(TargetName )
-
-    if (Tetris_AUTO_RUN_TEST)
-        set(${NAME}_AUTO_RUN_TEST TRUE)
-        set(${NAME}_AUTO_RUN_TESTS TRUE)
-    endif()
-    
-    set(TargetName ${NAME})
-    subdirs("Extern/${NAME}/${DIR}")
+macro(DefineExternalTargetEx NAME GROUP INCLUDE_PATH SOURCE_DIR BLD_TEST RUN_TEST)
+    set(${NAME}_ExternalTarget      TRUE)
+    set(${NAME}_TargetFolders       TRUE)
+    set(${NAME}_TargetName          ${NAME})
+    set(${NAME}_TargetGroup         ${GROUP})
+    set(${NAME}_FOLDER              ${GROUP})
+    set(${NAME}_INCLUDE             ${INCLUDE_PATH})
+    set(${NAME}_DIRECTORY           ${SOURCE_DIR})
+    set(${NAME}_LIBRARY             ${${NAME}_TargetName})
+    set(${NAME}_BUILD_TEST          ${BLD_TEST})
+    set(${NAME}_AUTO_RUN_TEST       ${RUN_TEST})
+    set(${NAME}_AUTO_RUN_TESTS      ${RUN_TEST})
 endmacro()
