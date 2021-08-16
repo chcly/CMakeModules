e0789af649f224fdc1195d81d0fe3514c2882c44
CharlesCarley <snailrose@gmail.com>
2021-08-16 01:15:33 -0600

    Update

--- FindVSEmscripten.cmake
+++ FindVSEmscripten.cmake
@@ -22,13 +22,13 @@
 string(COMPARE EQUAL "${CMAKE_SYSTEM_NAME}"  "Emscripten" _SystemNameEmscripten) 
 string(COMPARE EQUAL "${CMAKE_VS_PLATFORM_TOOLSET}"  "emsdk"  _SystemToolsetEmscripten)
 
+
 if(_SystemNameEmscripten OR _SystemToolsetEmscripten)
     set(USING_EMSCRIPTEN TRUE)
     
-    if (_SystemNameEmscripten)
+        if (_SystemNameEmscripten)
         set(USING_EMSCRIPTEN_STD TRUE)
     endif()
-
     include(VSEmscripten)
 else()
     set(USING_EMSCRIPTEN FALSE)
